49a3565c0f975a2017a8380a1d7ade3535315306
junp <junpmail@163.com>
2014-02-14 00:55:45 +0800

    update

--- check.js
+++ check.js
@@ -3,37 +3,30 @@ var querystring = require('querystring')
 var cookie = require('./cookie')
 var config = require('./config')
 
-var content = querystring.stringify({
-	card: config.card,
-	fromurl: '',
-	login_user: '',
-	password: config.password	
-})
+var content = ''
 
 var options = {
 	host: 'weixin.91160.com',
-	path: '/index.php?c=user&a=login',
-	method: 'POST',
+	path: '/index.php?c=account&a=index',
+	method: 'GET',
 	headers: {
 	"Content-Length": content.length,
-	"Content-Type": "application/x-www-form-urlencoded",
+	"Cookie": cookie.getCookie(),
 	"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 	"Accept-Encoding":"gzip, deflate",
 	"Accept-Language":"en-US,en;q=0.5",
 	"Connection":"keep-alive",
 	"Host":"weixin.91160.com",
-	"Referer":"http://weixin.91160.com/index.php?c=user&a=login",
+	//"Referer":"http://weixin.91160.com/index.php?c=user&a=login",
 	"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0"
 	}
 }
 
-var login = function(){
+var check = function(){
 
 
 	var req = http.request(options, function(res){
 		res.setEncoding('utf8')
-		var cookies = res.headers['set-cookie']
-		cookie.setCookie(cookies)
 		res.on('data', function(data){
 			console.log(data)
 		})
@@ -43,4 +36,4 @@ var login = function(){
 	req.end()
 }
 
-login()
+ check()
