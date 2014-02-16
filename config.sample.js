8bf3a98246fb4748bf310d230fe3a23508da7199
junp <junpmail@163.com>
2014-02-17 00:48:58 +0800

    update

--- config.sample.js
+++ config.sample.js
@@ -1,16 +1,13 @@
 var conf = {
-	login:{
-		card: '',
-		password: ''
-	},
-	
-	order: {
-		member_id: '',	// 挂号用户ID
-		pay_method: '2',		// 2.现场支付
-		unit_id: '103',			// 医院 103 中医院，100沙河
-		sch_id: '2595793',		// 排班
-		detl_id: '10136992'		// 号源
-	}
+	card: '',
+	password: '',
+	member_id: '',	// 挂号用户ID
+	pay_method: '2',		// 2.现场支付
+	unit_id: '103',			// 医院 103 中医院，100沙河
+	sch_id: '2595793',		// 排班
+	detl_id: '10136992',	// 号源
+	doc_id: '8003',			// 医生 8003 曹大农号
+	dep_id: '2534'			// 门诊 2534 妇科 
 
 }
 
