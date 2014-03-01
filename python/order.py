9648ad0a7e4212e6656d6618771fd533f3244df5
junp <junpmail@163.com>
2014-03-01 22:46:40 +0800

    update

--- python/order.py
+++ python/order.py
@@ -1,45 +1,95 @@
-#_*_encoding:utf8_*_
-import ConfigParser
+#_*_coding=utf8_*_
+import time
 import urllib
 import urllib2
-import cookielib
+import re
+import time
+import ConfigParser
 
-cf = ConfigParser.ConfigParser()
-cf.read("config.ini")
+class order():
+	def __init__(self):
+		self.initConfig()
 
-value = {
-	'member_id': cf.get('user', 'member_id'),
-	'pay_method': cf.get('pay', 'pay_method'),
-	'unit_id': cf.get('hospital', 'unit_id'),
-	'sch_id': cf.get('hospital', 'sch_id'),
-	'detl_id': cf.get('hospital', 'detl_id')
-}
+	#配置
+	def initConfig(self):
+		cf = ConfigParser.ConfigParser()
+		cf.read('config.ini')
+		
 
-data = urllib.urlencode(value)
+		#用户ID
+		self._member_id = cf.get('user', 'member_id')
 
-def order():
-	cookie = cookielib.LWPCookieJar()
-	cookie.load('cookie.txt')
-	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookie))
-	urllib2.install_opener(opener)
-	req = urllib2.Request('http://weixin.91160.com/index.php?c=order&a=submit')
+		#手机号
+		self._phone = cf.get('user', 'phone')
 
-	req.set_proxy(cf.get('proxy', 'host'), 'http')
-	req.add_header('Origin', 'http://weixin.91160.com')
-	req.add_header('Content-Type', 'application/x-www-form-urlencoded')
-	req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
-	req.add_header('Accept-Encoding', 'gzip, deflate')
-	req.add_header('Accept-Language', 'en-US,en;q=0.5')
-	req.add_header('Connection', 'keep-alive')
-	req.add_header('Host', 'weixin.91160.com')
-	req.add_header('Referer', "http://weixin.91160.com/index.php?c=order&a=confirm&unit_id="+cf.get('hospital','unit_id')+"&sch_id="+cf.get('hospital','sch_id')+"&detl_id="+cf.get('hospital','detl_id'))
-	req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0')
-	req.add_header('Content-Length', len(data))
+		#支付方式
+		self._pay_method = cf.get('pay', 'pay_method')
 
-	req.add_data(data)
-	res = urllib2.urlopen(req)
-	#print len(data)
-	print res.read()
+		#医生ID
+		self._doctor_id = cf.get('hospital', 'doctor_id')
+
+		#排班
+		self._sch_id = cf.get('hospital', 'sch_id')
+
+		#号源
+		self._detl_id = cf.get('hospital', 'detl_id')
+
+		# 验证码
+		self._captcha = cf.get('check', 'captcha')
+
+		#短信验证码
+		self._ny_sms_code = cf.get('check', 'ny_sms_code')
+
+		#手机验证
+		self._phone_check = cf.get('check', 'phone_check')
+
+		#cookie
+		self._cookie = cf.get('header', 'cookie')
+		
+
+	#提交
+	def submit(self):
+		value = {
+			'captcha': self._captcha,
+			'member_id': self._member_id,
+			'pay_method': self._pay_method,
+			'schid': self._sch_id,
+			'doctor_id': self._doctor_id,
+			'phone': self._phone,
+			'ny_sms_code': self._ny_sms_code,
+			'phone_check': self._phone_check,
+			'detlid': self._detl_id
+		}
+
+		data = urllib.urlencode(value)
+
+		#提交地址
+		req = urllib2.Request('http://sz.91160.com/guahao/ysubmit.html')
+
+		#req.set_proxy(proxy, 'http')
+		req.add_header('Origin', 'http://sz.91160.com')
+		req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+		req.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+		req.add_header('Accept-Encoding', 'gzip, deflate')
+		req.add_header('Accept-Language', 'en-US,en;q=0.5')
+		req.add_header('Connection', 'keep-alive')
+		req.add_header('Host', 'sz.91160.com')
+		req.add_header('Referer', 'http://sz.91160.com/guahao/ystep3.html')
+		req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 ')
+		req.add_header('Cookie', self._cookie)
+		req.add_header('Content-Length', len(data))
+		req.add_data(data)
+
+		try:
+			res = urllib2.urlopen(req)
+			html = res.read().decode('utf-8')
+
+			print html
+	
+		except :
+			print 'error'
 
 if __name__ == '__main__':
-	order()
+	myOrder = order()
+	myOrder.submit()
+
